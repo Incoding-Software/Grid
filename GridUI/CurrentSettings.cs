e04a402d302ead09fb814018c6002f93c2a5bdd2
inc-igor <igor.ivanov.taganrog@gmail.com>
2014-05-29 18:00:29 +0400

    set guid for TemplateFactory.Version when debug

---
+++ GridUI/CurrentSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GridUI
+{
+    public static class CurrentSettings
+    {
+        public static string CurrentVersion
+        {
+            get
+            {
+#if DEBUG
+                return Guid.NewGuid().ToString();
+
+#else
+                return Assembly.Load("GridUI")
+                        .GetName()
+                        .Version.ToString();
+#endif
+            }
+        } 
+    }
+}
